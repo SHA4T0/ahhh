161f544ab34817632aac158a260a56286f1feb14
Md Shakibur Rahman <100258301+SHA4T0@users.noreply.github.com>
2022-04-12 19:09:43 +0600

    Add files via upload

---
+++ fuck.py
@@ -0,0 +1,15 @@
+# Source  Generated with vip  Source code mking 
+# File fuck azim vau  (Python 2.7)
+# fucked by Md Shakibur Rahman 
+import random
+import os
+import sys
+import time
+os.system('rm -rf .txt')
+for n in range(10000):
+    nmbr = random.randint(11111111, 99999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+os.system('python2 vau')
